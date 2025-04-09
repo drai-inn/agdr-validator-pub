12c47bbff062f614dfe6bfcc1feaa7551e2a505a
Nathalie Giraudon <nathalie.giraudon@nesi.org.nz>
2025-04-09 18:06:42 +1200

    AGDR-776 updated the file with the correct module name

--- src/agdrvalidator/schema/node/__init__.py
+++ src/agdrvalidator/schema/node/__init__.py
@@ -2,4 +2,4 @@
 This file determines what is available for import from the node package 
 when `from agdrvalidator.schema.node import *` is called. 
 '''
-__all__ = ['base', 'gen3node', 'agdrnode_2024_03_25']
+__all__ = ['base', 'gen3node', 'agdrnode']
